d97a0077b98bd4b469a2defeb1f869ebd8fa7050
benjamin <dubus.benjamin1@gmail.com>
2015-05-05 15:04:10 +0200

    ajouté image profil utilis le dossier includes avec header et footer

---
+++ compte_utilisateur.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>  <!-- cette ligne sert à ce qu'on ai pas besoin d'ecrire le code des accents-->
+  <title>Votre compte personnel</title>
+
+</head>
+
+<body>
+
+    <?php include('header.php');?>
+	<fieldset>
+        <legend><b>Mon compte</b></legend>
+        <img src="Images/photo_profil_defaut.png" alt="photo de profil">
+    </fieldset>
+
+    <fieldset>
+        <legend><b>Mes denrées</b></legend>
+    </fieldset>
+
+	<?php include('footer.php');?>
+
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>  <!-- cette ligne sert à ce qu'on ai pas besoin d'ecrire le code des accents-->
+</head>
+
+<body>
+	<center>
+		<h4>Contacts</h4>
+		<p>Clément Bazin de Jessey</p>
+		<p>Adresse mail : clembade@hotmail.fr</p>
+	</center>
+</body>
+</html>
