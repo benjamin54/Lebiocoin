5ce704cceee30061a7f8b725a488d25fc74eba9e
Drakorcarnis <cldejessey@hotmail.fr>
2015-05-05 11:57:20 +0200

    merge et reorganisation css

---
+++ maquettes/4ème-maquette-page-d'accueil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>page d'accueil</title>
+		<link rel="stylesheet" type="text/css" href="styles/page-d'accueil.css">
+  	</head>
+	<body>
+  		<nav>
+  			<ul>
+	  			<li>ACCUEIL</li>
+	  			<li>LEGUMES</li>
+	   			<li>FRUITS</li>
+	   			<li>LABEL BIO<img src="http://upload.wikimedia.org/wikipedia/fr/thumb/f/fc/AB.svg/728px-AB.svg.png" width="1%" height="1%" alt="Label Bio"></a></li>
+	  			<li>RECETTES</li>
+	   			<li>FORUM</li>
+	   			<li>AIDE</li>
+  			</ul>
+	  	</nav>
+	  	<div id="bandeau">
+	  		<div id="logo">
+	  			<img src="file:///C:/Users/Cl%E9ment/Documents/GitHub/Lebiocoin/logo.png" style="height:180px">
+	  		</div>
+	  		<div id="nomdusite">
+		  		 <ul>
+		  		 	<li style="font-size:50px; font-style:italic">LeBioCoin</li>
+		  		 	<li>ECHANGE DE FRUITS ET LEGUMES</li>
+		  		 	<li>depuis 2015</li>
+		  		 </ul>
+	  	</div>
+	  	
+	  	<div id="agauche">
+	  	</div>
+	  	<div class="onglet1">
+	  		<p>ACHAT</p>
+	  	</div>
+	  	<div class="onglet2">
+	  		<p>ECHANGE</p>
+	  	</div>
+	  	<div id="rechercher">
+	  	<div id="annonces">
+	  	<div class="actus">
+	  		<div id="actu1">
+	  		</div>
+	  		<div id="actu2">
+	  		</div>
+	  		<div id="actu3">
+	  		</div>
+	  		<div id="actu4">
+	  		</div>
+	  	</div>
+	  	<div id="bandeaupubs">
+	  	</div>
+	  	<div id="adroite">
+	  	</div>
+	  	<footer>
+	  		<div id="Pays">
+	  		</div>
+	  		<ul>
+	  			<li>CONTACTS</li>
+	  			<li>admin 1</li>
+	  			<li>admin 2</li>
+	  			<li>adresse mail</li>
+	  		</ul>
+	  	</footer>
+	</body>
+</html>
+
