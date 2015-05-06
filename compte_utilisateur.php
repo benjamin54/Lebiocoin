21771551be76034b7864e4dd3c82c874f854b2bd
benjamin <dubus.benjamin1@gmail.com>
2015-05-06 16:26:34 +0200

    maj mineure

--- compte_utilisateur.php
+++ compte_utilisateur.php
@@ -2,23 +2,23 @@
 <html>
 <head>
 	<meta charset="utf-8"/>  <!-- cette ligne sert à ce qu'on ai pas besoin d'ecrire le code des accents-->
+    <link rel="stylesheet" href="styles/accueil.css">
   <title>Votre compte personnel</title>
-
 </head>
 
 <body>
 
-    <?php include('header.php');?>
+    <?php include('includes/header.php');?>
 	<fieldset>
-        <legend><b>Mon compte</b></legend>
+        <legend><b>Mon profil</b></legend>
         <img src="Images/photo_profil_defaut.png" alt="photo de profil">
     </fieldset>
 
     <fieldset>
-        <legend><b>Mes denrées</b></legend>
+        <legend><b>Dernières actions</b></legend>
     </fieldset>
 
-	<?php include('footer.php');?>
+	<?php include('includes/footer.php');?>
 
 </body>
 </html>
