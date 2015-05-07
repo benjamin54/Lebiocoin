27fece1db0d703821bf5944678191f0600ec4bdd
Drakorcarnis <cldejessey@hotmail.fr>
2015-05-07 11:49:43 +0200

    ajout page contact

---
+++ contact.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" href="styles/accueil.css">
+  <title>contact</title>
+  <?php include("includes/header.php"); ?>
+</head>
+<body>
+
+</body>
+<?php include("includes/footer.php"); ?>
+</html>
