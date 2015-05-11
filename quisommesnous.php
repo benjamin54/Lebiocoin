fcf70d52c6073d0067ae4bab955effae0d0f2512
Drakorcarnis <cldejessey@hotmail.fr>
2015-05-11 18:47:21 +0200

    maj

--- quisommesnous.php
+++ quisommesnous.php
@@ -6,14 +6,7 @@
   <?php include("\includes/header.php"); ?>
 </head>
 <body>
-<div id="contenuaccueil">
-<h1>CONTACT</h1>
-<p>cldejessey@hotmail.fr</p>
-<p>dubus.benjamin1@gmail.com</p>
-<p>margaux@pasquier-desvignes.com</p>
-<p>stellinaepanda@gmail.com</p>
-<p>romain.merle91@gmail.com</p>
-</div>
+
 </body>
 <?php include("includes/footer.php"); ?>
 </html>
