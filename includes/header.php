738f32973ba4909203fad28d7736bfe81cbeeec4
Gomar23 <margaux@pasquier-desvignes.com>
2015-05-18 23:49:11 +0200

    Ajout du header php et css

--- includes/header.php
+++ includes/header.php
@@ -1,71 +1,96 @@
 <!DOCTYPE html>
 <html>
 <head>
+
   <meta charset="utf-8"/>
   <title>LeBioCoin</title>
-  <link rel="icon" type="image/png" href="Images/favicon-32x32.png" sizes="32x32"/>
-  <link rel="stylesheet" href="styles/header.css"/>
-  <link rel="stylesheet" href="styles/accueil.css"/>
+  <link rel="stylesheet" href="header.css">
+
 </head>
 
 <body>
 
-  <div id="hautdepage"><p><a href="connexion.php">Je me connecte</a> <a href="inscription.php">Je m'inscris</a></p></div>
-<p><p1>LeBioCoin</p1>
-  <div id="logo"><img src="Images/logo1.png"></img></div>
- <h3> Site de ventes de fruits & légumes</h3>
+  <div id="menu">
+        <ul height="100" width="500">
+          <li class="menu1"><a href ="pagedaccueil.php">ACCUEIL</a></li><!--
+          --><li class="menu1">LEGUMES</li><!--
+          --><li class="menu1"><a href="fruit.php">FRUITS</a></li><!--
+          --><li class="menu1">LABEL BIO</li><!--
+          --><li class="menu1">RECETTES</li><!--
+          --><li class="menu1">FORUM</li>
+          </ul>
+  </div>
 
-<div id="pos"> <img src="Images/fruits.jpg"></img></div>
-    <nav>
-        <ul><b>
-          <li><a href ="pagedaccueil.php">ACCUEIL</a></li>
-          <li>LEGUMES</li>
-          <li><a href="fruit.php">FRUITS</a></li>
-          <li>LABEL BIO</li>
-          <li>RECETTES</li>
-          <li>FORUM</li>
-          </b>
-        </ul>
-    </nav>
-</div>
-
-<div id=page>
- 
- <div id="posi"> <img src="icone.jpg"></img></div>
-  <div id="posi2"> <img src="panier.jpg"></img></div>
-  <div id="menu"></div>
-</div>
-<div id="h"></div>
-<div id="gauche">
-
-</div>
+  
+  <div id="photo">
+      <img src="http://commentplaire.com/wp-content/uploads/2014/08/Fruits-and-Vegetable-Wallpapers-HD-1160x652.jpg" height="1500" width="1368"></img>
+  </div>
 
 
-<div id="recherche">
-<input type="texte" name="recherche" value="Je recherche..."/>
-<input type="submit" value="OK"/> 
-</div>
-<b>
-<ul> 
-  <div id="bordureg">
+  
 
-<li>  Annonces échanges </li> <br><br>
-<li>  Annonces Ventes </li><br><br>
-<li> <a href="annonce.php">Je crée mon annonce</a>  </li> <br><br>
-<li> Mentions legales</li><br><br>
-<li>  Aide </li> 
-</ul>
-</b>
-</div>
-<div id="newsletter"><h4><u>LA NEWSLETTER</u></h4>
-<p><input type="text" name="pseudo" value="rentrer votre mail" /> </p>
- <p><INPUT TYPE="submit" NAME="Valider" VALUE=" Envoyer"></p>
+  <div id="bloc1">
 
-</div>
+          <div id="nom">
+            <h1>LeBioCoin</h1>
+          </div>
+
+          <div id="logo">
+            <img src="logo3.PNG" width="160" height="150"></img>
+          </div>
+
+          <div id="nom2">
+            <p> Site de vente de fruits & légumes</p>
+          </div>
+
+  </div>
 
 
-<div id="mag"><h4><u>LA REVUE</u></h4>
-  <img src="mag.png"></img>
-</div>
+  
+
+  <div id="bloc2">
+
+          <div id="recherche">
+            <form action=" ">
+              <input class="barre" type="search" name="recherche" placeholder="Rechercher"/>
+              <input class="bouton" type="submit" value="OK"/>
+            </form> 
+          </div>
+
+
+          <div id="menu_vertical">
+            <ul>
+              <li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Actualités</a></li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Annonce</a><li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Panier</a></li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Compte</a></li><!--
+             --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Déconnexion</a></li>
+            </ul>
+          </div>
+
+  </div>
+
+
+
+
+  <div id="bloc3">
+
+          <div id="newsletter">
+            <p class="titre_footer1">LA NEWSLETTER</p>
+            <form action="news">
+              <input type="text" name="pseudo" placeholder="Rentrer votre email" />
+              <input type="bouton" name="submit" value=" Envoyer"/>
+            </form>
+          </div>
+
+          
+          <div id="revue">
+            <p class="titre_footer2">LA REVUE</p>
+          </div>
+
+  </div>
+
+
 </body>
+
 </html>
