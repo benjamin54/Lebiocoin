22f54e9086b99662dd710eaf6f27be7ce8e017f8
Clément <cldejessey@hotmail.fr>
2015-05-19 10:20:39 +0200

    maj header

--- includes/header.php
+++ includes/header.php
@@ -60,7 +60,7 @@
           <div id="menu_vertical">
             <ul>
               <li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Actualités</a></li><!--
-            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Annonce</a><li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Annonce</a></li><!--
             --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Panier</a></li><!--
             --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Compte</a></li><!--
              --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Déconnexion</a></li>
