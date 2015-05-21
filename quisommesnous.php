d64a295e79b69f6f293d3f394e07f074fda924f8
Clément <cldejessey@hotmail.fr>
2015-05-21 11:42:42 +0200

    maj

--- quisommesnous.php
+++ quisommesnous.php
@@ -1,12 +1,17 @@
-<<!DOCTYPE html>
-<html>
-<head>
+<?php include("header.php"); ?>
  <link rel="stylesheet" href="styles\includes/header.css">
-  <title>contact</title>
-  <?php include("\includes/header.php"); ?>
-</head>
-<body>
+<div id="quisommesnous">
+<h4> LE CONCEPT </h4>
+<p>CONSOMMER SAIN EN CREANT DU LIEN SOCIAL...</p>
+<p>LEBIOCOIN est un site d’échange et de ventes de fruits et légumes frais.</p>
+<p> Les particuliers peuvent échanger ou vendre des fruits et légumes de leur potager </p>
+<p>Il a pour but de : </p>
+<p>-redonner de l’importance et de la valeur aux fruits et légumes </p> 
+<p>-privilégier la qualité et fraicheur des produits. Mais comment ? </p>
+<p>L’achat ou l’échange s’effectue entre deux producteurs à proximité </p>
+<p> Le nombre de kilomètre parcourus par les produits étant réduits,</p>
+<p> leur fraicheur et qualité en sera meilleur.</p>
+<h4>LE PANIER</h4>
+<p> LE BIOCOIN s’occupe <u>gratuitement</u> du contact entre les particuliers qui aura lieu par mail
+</div>
 
-</body>
-
-</html>
