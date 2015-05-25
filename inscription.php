06d99fe6cab6f29e17925b49b5e907c1091c3b9c
Gomar23 <margaux@pasquier-desvignes.com>
2015-05-25 18:51:41 +0200

    remise en forme inscription

--- inscription.php
+++ inscription.php
@@ -90,18 +90,12 @@
 
         
                   
-                 
-
-
-           
-            
-            
-             $base = mysqli_connect ('localhost', 'root', '');  
+          $base = mysqli_connect ('localhost', 'root', '');  
               mysqli_select_db ($base,'mabase') ;
  
             //On prépare la commande sql d'insertion
       
-             $sql = 'INSERT INTO membre VALUES ("","'.$prenom.'","'.$nom.'","'.$adressemail.'", "'.$motdepasse1.'",   "'.$motdepasse2.'",     "'.$ville.'",   "'.$num.'",  "'.$photo.'","'.$case.'")'; 
+          $sql = 'INSERT INTO membre VALUES ("","'.$prenom.'","'.$nom.'","'.$adressemail.'", "'.$motdepasse1.'",   "'.$motdepasse2.'",     "'.$ville.'",   "'.$num.'",  "'.$photo.'","'.$case.'")'; 
               //"'.$adressemail.'", "'.$motdepasse1.'", "'.$motdepasse2.'", "'.$adresse.'",   "'.$codepostal.'", "'.$ville.'","'.$choix.'","'.$num.'")'; 
  
             /*on lance la commande (mysql_query) et au cas où, 
@@ -114,60 +108,91 @@
         }
         
         ?>
-</div>
+  </div>
 
 
-<div id=page>
+  <div id="page">
  
- <div id="posi"> <img src="icone.jpg"></img></div>
-  <div id="posi2"> <img src="panier.jpg"></img></div>
-  <div id="menu"></div>
-</div>
-<div id="h"></div>
-<div id="gauche">
-
-</div>
-
-
-<div id="recherche">
-<input type="texte" name="recherche" value="Je recherche..."/>
-<input type="submit" value="OK"/> 
-</div>
-<b>
-<ul> 
-  <div id="bordureg">
-
-<li>  Annonces échanges </li> <br><br>
-<li>  Annonces Ventes </li><br><br>
-<li> <a href="annonce.php">Je crée mon annonce</a>  </li> <br><br>
-<li> Mentions legales</li><br><br>
-<li>  Aide </li> 
-</ul>
-</b>
-</div>
-<div id="newsletter"><h4><u>LA NEWSLETTER</u></h4>
-<p><input type="text" name="pseudo" value="rentrer votre mail" /> </p>
- <p><INPUT TYPE="submit" NAME="Valider" VALUE=" Envoyer"></p>
-
-</div>
+    <div id="posi">
+      <img src="icone.jpg"></img>
+    </div>
+    
+    <div id="posi2">
+      <img src="panier.jpg"></img>
+    </div>
+  
+    <div id="menu">
+    </div>
+  
+  </div>
 
 
-<div id="mag"><h4><u>LA REVUE</u></h4>
-  <img src="mag.png"></img>
+  <div id="h">
+  </div>
 
- <p><input type="text" name="pseudo" value="rentrer votre mail" /> </p>
- <p><INPUT TYPE="submit" NAME="Valider" VALUE=" Envoyer"></p>
- 
- <div id="liste">
+  <div id="gauche">
+  </div>
 
 
-</div>
+  <div id="recherche">
+
+    <input type="texte" name="recherche" value="Je recherche..."/>
+    <input type="submit" value="OK"/> 
+  </div>
+
+  <b>
+  
+  <ul> 
+    <div id="bordureg">
+
+      <li>  Annonces échanges </li>
+      <br>
+      <br>
+      <li>  Annonces Ventes </li>
+      <br>
+      <br>
+      <li> <a href="annonce.php">Je crée mon annonce</a> </li>
+      <br>
+      <br>
+      <li> Mentions legales</li>
+      <br>
+      <br>
+      <li> Aide </li>
+
+     </div>  
+  </ul>
+
+  </b>
+
+   
+  <div id="newsletter">
+    <h4><u>LA NEWSLETTER</u></h4>
+
+    <p><input type="text" name="pseudo" value="rentrer votre mail" /> </p>
+    <p><INPUT TYPE="submit" NAME="Valider" VALUE=" Envoyer"></p>
+
+  </div>
 
 
-</div> 
+  <div id="mag"> 
+
+    <h4><u>LA REVUE</u></h4>
+  
+    <img src="mag.png"></img>
+
+    <p><input type="text" name="pseudo" value="rentrer votre mail" /> </p>
+    <p><INPUT TYPE="submit" NAME="Valider" VALUE=" Envoyer"></p>
+  
+  </div> 
+  
+  <div id="liste">
+  </div>
+
+
+  
+
 </body>
-</div>
-</div>
+
 
 </html>
 
