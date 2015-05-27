375fbf5cec1572bb763490c6c35adefdb31a5e07
Gomar23 <margaux@pasquier-desvignes.com>
2015-05-27 18:56:04 +0200

    Inscription plus sécurisée

--- inscription.php
+++ inscription.php
@@ -1,81 +1,251 @@
 <!DOCTYPE html>
+
+<?php
+
+$bdd = new PDO ('mysql:host=127.0.0.1;dbname=bdlebiocoin','root','');
+
+  
+
+  if(isset($_POST['forminscription']))
+  {
+     $prenom = htmlspecialchars($_POST['prenom']);
+      $nom = htmlspecialchars($_POST['nom']);
+      $pseudo = htmlspecialchars($_POST['pseudo']);
+      $adressemail = htmlspecialchars($_POST['adressemail']);
+      $motdepasse1 = sha1($_POST['motdepasse1']);
+      $motdepasse2 = sha1($_POST['motdepasse2']);
+      $region = htmlspecialchars($_POST['region']);
+      $ville = htmlspecialchars($_POST['ville']);
+      $case = htmlspecialchars($_POST['case']);
+
+      if(!empty($_POST['prenom']) AND !empty($_POST['nom']) AND !empty($_POST['pseudo']) AND !empty($_POST['adressemail']) AND !empty($_POST['motdepasse1']) AND !empty($_POST['motdepasse2']) AND 
+      !empty($_POST['num']) AND !empty($_POST['region']) AND !empty($_POST['ville']) AND !empty($_POST['case']))
+      {
+
+          $reqpseudo = $bdd -> prepare("SELECT * FROM membre WHERE pseudo = ?");
+          $reqpseudo -> execute(array($pseudo));
+          $pseudoexist = $reqpseudo -> rowCount('');
+
+              if($pseudoexist == 0)
+              {
+
+                 $motdepasse1length = strlen($motdepasse1);
+                  if($motdepasse1length >= 8)
+                  {
+                        if($motdepasse1 == $motdepasse2)
+                        {
+                                  if(filter_var($motdepasse1, FILTER_VALIDATE_EMAIL))
+                                  {
+
+                                        $reqmail = $bdd -> prepare("SELECT * FROM membre WHERE adressemail = ?"); /* requete qui permet des sélectionner toutes les entrées de la table membre ou le mail 
+                                        etait egal au mail que l'utilisateur a enregistré, et donc ca permet de verifier si le mail existe deja*/
+                                        $reqmail -> execute(array($adressemail));
+                                        $mailexist = $reqmail -> rowCount(''); /* rowCount compte le nombre de colonnes qui existe pour ce qu'on a rentré avant*/
+                                    
+                                            if($mailexist == 0)
+                                            {
+
+                                              $insertmembre = $bdd -> prepare("INSERT INTO membre (prenom, nom, pseudo, adressemail, motdepasse1, motdepasse2, num, region, ville, case) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"); 
+                                            /* INSERT INTO est une fonction sql et il ne faut pas oublier de la preparer en mettant le prepare*/                                        
+                                              $insertmembre -> execute(array($prenom, $nom, $pseudo, $adressemail, $motdepasse1, $num, $region, $ville, $case));
+                                              $message = "Votre compte a bien été créé. Bienvenue !";
+                                           }
+
+                                           else 
+                                           {
+                                            $erreur = "Adresse email déjà utilisée !";
+                                           }
+
+                                  }
+
+                                  else
+                                  {
+                                    $erreur = "Votre email n'est pas valide !";
+                                  } 
+                        }
+
+
+                        else
+                        {
+                          $erreur = "Vos mots de passe ne correspondent pas !";
+                        }
+                  }
+
+                  else
+                  {
+                    $erreur = "Le mot de passe doit contenir au minimum 8 caractères !";
+                  }
+              }
+
+              else
+              {
+                $erreur = "Votre pseudo a déjà été utilisé !";
+              }
+      }
+      
+      else 
+      {
+        $erreur = "Tous les champs doivent être complétés !";
+      }
+
+  
+
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <html>
 <head>
+
     <meta charset="utf-8"/>
     <title>LeBiocoin</title>
     <link rel="stylesheet" href="styles/header.css">
     <?php include("header.php"); ?>
+
  </head>
-  <div id="contenuaccueil">
-    <p>
-      <h4><CENTER>INSCRIPTION</CENTER></h4>
-    </p>
-    <p><font style="Verdana"> Veuillez remplir le formulaire ci dessous. Les champs  <p2 style="color:red">*</p2> sont obligatoires.</font></p>
-
-          
-    <h4> <CENTER>Mes identifiants</CENTER> </h4>
-
-    <form name="inscription" method="post" action="inscription.php">
-
-      <p><p2 style="color:red">*</p2>nom: <input type="text" name="nom" />
-
-<form name="inscription" method="post" action="1.php">
-<p><p2 style="color:red">*</p2>nom:
- <input type="text" name="nom" />
-  <p2 style="color:red">*</p2>prenom: <input type="text" name="prenom" /></p>
-
-      <p2 style="color:red">*</p2>prénom: <input type="text" name="prenom" /></p>
-
-      <p><p2 style="color:red">*</p2>Adresse mail: <input type="text" name="adressemail" value=""/></p>
-
-      <p><p2 style="color:red">*</p2>Mot de passe: <input type="password" name="motdepasse1"/><p2 style="color:red">(8 caract&egraveres minimum)</p2> </p>
-
-      <p><p2 style="color:red">*</p2>Confirmer votre mot de passe: <input type="password" name="motdepasse2"/></p>
 
 
-    <h4><CENTER>Mes coordonées de livraisons</CENTER></h4>
+<body>
 
-      <p>
-      <p>
-        <p2 style="color:red">*</p2>Ville  <input type="text" name="ville" />
-      </p>
-      <p>
+    <div id="contenuaccueil" align="center">
+        <form method="POST" action="">
+          <fieldset>
+            <legend><h2>INSCRIPTION</h2></legend>
+              
+                <br/>
+                <p>Veuillez remplir le formulaire ci dessous. Les champs <p2 style="color:red">*</p2> sont obligatoires.</p>
+                <br/>
+              
+                <h3>Mes identifiants</h3>
 
-      </select>
+                  
+                    <table>
+                        <tr>
+                          <td align="right">
+                              <label for="prenom">Prénom :</label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Votre prénom" id="prenom" name="prenom" >
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="right">
+                              <label for="nom">Nom :</label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Votre nom" id="nom" name="nom" >
+                          </td>
+                        </tr> 
+                        <tr>
+                          <td align="right">
+                              <label for="pseudo">Pseudo :</label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" >
+                          </td>
+                        </tr> 
+                          <td align="right">
+                              <label for="mail">Mail :</label>
+                          </td>
+                          <td>
+                              <input type="email" placeholder="Votre email" id="mail" name="adressemail">
+                          </td>
+                        </tr>  
+                          <td align="right">
+                              <label for="mdp">Mot de passe :</label>
+                          </td>
+                          <td>
+                              <input type="password" placeholder="Votre mot de passe" id="mdp" name="motdepasse1">
+                          </td>
+                         </tr> 
+                          <td align="right">
+                              <label for="mdp2">Confirmation du mot de passe :</label>
+                          </td>
+                          <td>
+                              <input type="password" placeholder="Confirmer le mot de passe" id="mdp2" name="motdepasse2">
+                          </td>
+                        </tr>
+                    </table>
+                 
 
-      <p><p2 style="color:red">*</p2>Num&eacutero de fixe/Mobile<input type="text" name="num" /></p>
+              <br>                
 
-      <p><p2 style="color:red">*</p2>Choix d'une photo de profil: <input type="file" name="photo"/> </p>
+                  <h3>Mes coordonées de livraison</h3>
+                    
+               
+                      <table>
+                        <tr>
+                          <td align="right">
+                              <label for="numero">Votre numéro de téléphone :</label>
+                          </td>
+                          <td>
+                              <input type="int" value="" id="numero" name="num">
+                          </td>
+                        </tr>  
+                          <td align="right">
+                              <label for="region">Région :</label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Choisissez votre région" id="region" name="region">
+                          </td>
+                        </tr>
+                          <td align="right">
+                              <label for="ville">Ville :</label>
+                          </td>
+                          <td>
+                              <input type="text" placeholder="Votre Ville" id="ville" name="ville">
+                          </td>
+                        </tr>
+                      </table>
 
-      <p><u>Accepter les conditions d'utilisations du BIOCOIN</u><p>
+                    <br>
+
+                    <p>J'accepte les conditions d'utilisation du BIOCOIN :<input type="checkbox" name="case" value="case"></p>
+                                    
+                              <p>"Je suis âgé(e) d'au moins 18 ans."</p>
+                          
+                              <p>"La transaction se fait en main propre, car le site a pour vocation de créer du lien localement."</p>
+                           
+                              <br>
+
+                        <CENTER><input type="submit" name="valider"  name="confirminscription" value=" Valider mon inscription"></CENTER>
+
+                 
 
 
-      <p>"Je suis ag&eacute d'au moins 18 ans"</p>
-      <p>"La transaction se fait en main propre, car le site a pour vocation de cr&eacuteer du lien localement"</p>
-      <p><input type="checkbox" name="case" id="case" /> <label for="case"> </label>
-      <p2 style="color:red">*</p2>J'accepte ces conditions</p>
+    <?php
+      if(isset($erreur))
+      {
+        echo '<font-color="red">' .$erreur. "</font>";
+      }
+      if(isset($message))
+      {
+        echo '<font-color="blue">' .$message. "</font>";
+      }
+    ?>
+
+            </fieldset>
+          </form>
 
 
 
-      <CENTER><p> <input type="submit" name="valider"  value=" Valider ma création de compte"></p></CENTER>
+        
 
-
-    </form>
-
-<?php
-        if (isset ($_POST['valider']))  
-        {
-            //On récupère les valeurs entrées par l'utilisateur :
-            
-            $prenom=$_POST['prenom'];
-            $nom=$_POST['nom'];
-            $adressemail=$_POST['adressemail'];
-            $motdepasse1=$_POST['motdepasse1'];
-            $motdepasse2=$_POST['motdepasse2'];
-            $ville=$_POST['ville'];
-            $num=$_POST['num'];
-            $photo=$_POST['photo'];
-            $case=$_POST['case'];      
+<!--
+                 
               
                 
   
@@ -108,37 +278,10 @@
         }
         
         ?>
+
+      -->
   </div>
 
-
-  <div id="page">
- 
-    <div id="posi">
-      <img src="icone.jpg"></img>
-    </div>
-    
-    <div id="posi2">
-      <img src="panier.jpg"></img>
-    </div>
-  
-    <div id="menu">
-    </div>
-  
-  </div>
-
-
-  <div id="h">
-  </div>
-
-  <div id="gauche">
-  </div>
-
-
-  <div id="recherche">
-
-    <input type="texte" name="recherche" value="Je recherche..."/>
-    <input type="submit" value="OK"/> 
-  </div>
+</body>
 
 </html>
-
