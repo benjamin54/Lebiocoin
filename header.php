ad1383d89b100084f948dd5aafec216cc1066633
Clément <cldejessey@hotmail.fr>
2015-05-28 12:01:14 +0200

    maj header

--- header.php
+++ header.php
@@ -42,7 +42,15 @@
 
   </div>
 
-
+ <div id="menu_vertical">
+            <ul>
+              <li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Actualités</a></li><!--
+            --><li class="menu2"><a href="annonce.php">Déposer Annonce</a></li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Panier</a></li><!--
+            --><li class="menu2"><a href="compte_utilisateur.php">Compte</a></li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Déconnexion</a></li>
+            </ul>
+ </div>
   
 
   <div id="bloc2">
@@ -53,18 +61,6 @@
               <input class="bouton" type="submit" value="OK"/>
             </form> 
           </div>
-
-
-          <div id="menu_vertical">
-            <ul>
-              <li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Actualités</a></li><!--
-            --><li class="menu2"><a href="annonce.php">Déposer Annonce</a></li><!--
-            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Panier</a></li><!--
-            --><li class="menu2"><a href="compte_utilisateur.php">Compte</a></li><!--
-            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Déconnexion</a></li>
-            </ul>
-          </div>
-
   </div>
 
 
