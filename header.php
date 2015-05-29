7a3eb6a87c2ee0b42b99b6c60f3cc52043d5e273
Gomar23 <margaux@pasquier-desvignes.com>
2015-05-29 19:38:00 +0200

    maj header

--- header.php
+++ header.php
@@ -63,8 +63,8 @@ function bascule(elem)
             <ul>
               <li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Actualités</a></li><!--
             --><li class="menu2"><a href="annonce.php">Déposer Annonce</a></li><!--
-            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Panier</a></li><!--
-            --><li class="menu2"><a href="compte_utilisateur.php">Compte</a></li><!--
+            --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Mon Panier</a></li><!--
+            --><li class="menu2"><a href="compte_utilisateur.php">Mon Compte</a></li><!--
             --><li class="menu2"><a href="http://www.creativejuiz.fr/blog/theme/css-css3">Déconnexion</a></li>
             </ul>
  </div>
