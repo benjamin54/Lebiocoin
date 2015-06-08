89833dd32342fc1036620828fc154e2c613b7c4a
jonathanberthelot <jonathan92100@gmail.com>
2015-06-08 22:16:52 +0200

    rechercheavancee2.0

---
+++ rechercheavsuite.php
@@ -0,0 +1,88 @@
+
+
+    <head>
+        
+        <base href="http://www.lesfruitsetlegumesfrais.com/" />
+        <title>Fruits &amp; Légumes - Les fruits et légumes frais</title>
+        <meta charset="utf-8"/>
+  <link rel="stylesheet" href="accueil.css">
+   </head>
+
+    <body >
+       
+<div id="header">
+
+    <div id="logo">
+        <a href="/">
+            <img src="images/common/logo.png" alt="" />
+        </a>
+    </div>
+    
+        <h1 class="h1-like">
+            Votre recherche : 
+                    </h1>
+           
+</div>
+<div class="matrice">
+    <div id="matriceContainer">
+        <div class="item bigTetris">
+    <a href="/fruits-legumes/petits-fruits-et-fruits-rouges/cassis/carte-identite">
+        
+        <img src="_upload/cache/ressources/produits/cassis/cassis_298_278_filled.jpg" alt="Cassis" />
+        <span>Cassis</span>
+    </a>
+</div>
+<div class="item">
+    <a href="/fruits-legumes/petits-fruits-et-fruits-rouges/framboise/carte-identite">
+        <img src="_upload/cache/ressources/produits/framboise/framboise_138_118_filled.jpg" alt="Framboise" />
+        <span>Framboise</span>
+    </a>
+</div>
+<div class="item">
+    <a href="/fruits-legumes/petits-fruits-et-fruits-rouges/mure/carte-identite">
+        <img src="_upload/cache/ressources/produits/mure/mure_138_118_filled.jpg" alt="Mûre" />
+        <span>Mûre</span>
+    </a>
+</div>
+<div class="item">
+    <a href="/fruits-legumes/petits-fruits-et-fruits-rouges/fraise/carte-identite">
+        <img src="_upload/cache/ressources/produits/fraise/fraise_138_118_filled.jpg" alt="Fraise" />
+        <span>Fraise</span>
+    </a>
+</div>
+<div class="item">
+    <a href="/fruits-legumes/petits-fruits-et-fruits-rouges/groseille/carte-identite">
+        <img src="_upload/cache/ressources/produits/groseille/groseille_138_118_filled.jpg" alt="Groseille" />
+        <span>Groseille</span>
+    </a>
+</div>
+<div class="item bigTetris">
+    <a href="/fruits-legumes/petits-fruits-et-fruits-rouges/myrtille/carte-identite">
+        
+        <img src="_upload/cache/ressources/produits/myrtille/myrtille_298_278_filled.jpg" alt="Myrtille" />
+        <span>Myrtille</span>
+    </a>
+</div>
+    </div>           
+</div>
+                   
+            </div>
+            <div id="footer">
+    
+     <div class="footer-nav">
+        Les fruits et légumes frais © Tous droits réservés        <a href="/qui-sommes-nous">Qui sommes-nous ?</a>
+        <a href="/liens">Liens</a>
+        <a href="/nous-contacter">Contact</a>
+        <a href="/mentions-legales">Mentions légales</a>
+    </div>
+    
+    <div class="row map">
+        <div class="cell">
+            <h2 class="h2-like">
+                <img src="images/common/titre-les-fruits.png" alt="Les fruits" />
+            </h2>
+              
+
+    </body>
+
+</html>
