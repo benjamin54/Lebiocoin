399c43601b50313694b9252a21df95a4d6996a02
benjamin54 <dubus.benjamin1@gmail.com>
2015-06-09 23:10:26 +0200

    integration table chat

--- Base-de-donnees/mabase.sql
+++ Base-de-donnees/mabase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 08 Juin 2015 à 19:58
+-- Généré le :  Mar 09 Juin 2015 à 23:08
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -166,6 +166,29 @@ CREATE TABLE IF NOT EXISTS `categorie_sujet` (
   `nom_categoriesujet` varchar(255) NOT NULL,
   PRIMARY KEY (`id_categoriesujet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chat`
+--
+
+CREATE TABLE IF NOT EXISTS `chat` (
+  `id_chat` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id_chat`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `chat`
+--
+
+INSERT INTO `chat` (`id_chat`, `pseudo`, `message`) VALUES
+(1, 'stelloh', 'coucpu\r\n'),
+(2, 'aude', ''),
+(3, 'aude   ', 'ca va'),
+(4, 'aude   ', 'ca va');
 
 -- --------------------------------------------------------
 
