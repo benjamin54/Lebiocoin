c0924924099a46507dea43254fbcc02d9fe867c2
Gomar23 <margaux@pasquier-desvignes.com>
2015-06-11 00:01:20 +0200

    maj headeradmin

--- headeradmin.php
+++ headeradmin.php
@@ -21,89 +21,86 @@ else{document.getElementById(elem).style.visibility="hidden";}
 
 
 
-	<div id="menu">
-        <ul>
-             <li class="menu1"><a href ="pagedaccueil.php">Accueil</a></li><!--
-          --><li class="menu1"><a href="annonce_vue.php">Annonces Légumes</li><!--
-          --><li class="menu1"><a href="annonce_vue.php">Annonces Fruits</a></li><!--
-          --><li class="menu1"><a href="forum/index.php">Forum</a></li><!--
-          --><li class="menu1"><a>Recettes</a></li><!--
-          -->
-          </ul>
-  </div>	  
+	<body>
+<div id="menu">
+    <ul>
+             <li class="menu1"><a href ="pagedaccueil.php">ACCUEIL</a></li><!--
+          --><li class="menu1"><a href="annonce_vue.php">ANNONCE LEGUMES</li><!--
+          --><li class="menu1"><a href="annonce_vue.php">ANNONCE FRUITS</a></li><!--
+          --><li class="menu1"><a href="forum/index.php">FORUM</a></li><!--
+          --><li class="menu1"><a href="rechercheavancee2.0.php">RECHERCHE AVANCEE</a></li><!--
+      -->
+  </ul>
+</div>    
 
-  <div id="bloc1">
+  <div id="connexion" onClick="bascule('connex');"><a href="connexion2.0.php">Connexion</a></div>
 
-          <div id="nom">
-            <h1>LeBioCoin</h1>
-          </div>
+  <div id="inscription"><a href="inscription.php">Inscription</a></div>
 
-          <div id="logo">
-            <img src="Images/logo.PNG" width="160" height="155"></img>
-          </div>
-
-          <div id="nom2">
-            <p> Site de vente de fruits & légumes</p>
-          </div>
-
-  </div>
-
- <div id="menu_vertical">
-            <ul>
-              <li class="menu2"><a href="listemembre.php">Gerer les membres</a></li><!--
-            --><li class="menu2"><a href="listeannonce.php">Gérer les annonces</a></li><!--
-            --><li class="menu2">Publier une annonce</li><!--
-            --><li class="menu2"><a href="">Ajouter une catégorie de fruits/légumes</a></li><!--
-            --><a href="deconnexionadmin.php"><li class="menu2">Déconnexion</li></a>
-            </ul>
- </div>
   
+<div id="bloc1">
 
-  <div id="bloc2">
+  <div id="nom">
+    <h1>LeBioCoin</h1>
+  </div>
 
-          <div id="recherche">
-            <form >
-              <tr>
-              <input class="barre" type="search" name="recherche" placeholder="Rechercher"/>
-              <input class="bouton" type="submit" value="OK"/>
-              </tr>
-            </form> 
-          </div>
+  <div id="logo">
+    <a href="pagedaccueil.php"><img src="Images/logo.PNG" width="160" height="170"></img></a>
+  </div>
+
+  <div id="nom2">
+    <p> Site de vente de fruits & légumes</p>
+  </div>
+
+</div>
+
+<div id="menu_vertical">
+  <ul>
+              <li class="menu2"><a href="chat.php">Discussion membres</a></li><!--
+            --><li class="menu2"><a href="annonce.php">Déposer Annonce</a></li><!--
+            --><li class="menu2"><a href="inscription.php">Mes contacts</a></li><!--
+            --><li class="menu2"><a href="profil.php">Mon Compte</a></li><!--
+        --><li class="menu2"><a href="inscription.php">Déconnexion</a></li>
+    </ul>
+</div>
+
+
+<div id="bloc2">
+
+<!--  <div id="recherche">
+    <form >
+      <input class="barre" type="search" name="recherche" placeholder="Rechercher"/>
+      <input class="bouton" type="submit" value="OK"/>
+    </form> 
+  </div>
+</div>
+-->
+
+
+
+<div id="bloc3">
+
+  <div id="newsletter">
+    <p class="titre_footer1">LA NEWSLETTER</p>
+    <form action="news">
+      <input type="text" name="pseudo" placeholder="Rentrer votre email" />
+      <input type="bouton" name="submit" value=" Envoyer"/>
+    </form>
   </div>
 
 
+  
+  <footer>
+    <center>
+      <ul>
+        <li><a href="contact.php">Contact/Demandes</a></li>
 
+        <li><a href="quisommesnous.php">Qui sommes-nous ?</a></li>
+      </ul>
+    </center>
+  </footer>
+</div>
 
-  <div id="bloc3">
-
-          <div id="newsletter">
-            <p class="titre_footer1">La Newsletter</p>
-            <form action="news">
-              <input type="text" name="pseudo" placeholder="Rentrer votre email" />
-              <input type="bouton" name="submit" value=" Envoyer"/>
-            </form>
-          </div>
-
-          
-          <div id="revue">
-            <p class="titre_footer2">La Revue</p>
-          </div>
-
-          <footer>
-      			<center>
-      				<ul>
-      					<li><a href="contact.php">Contacts</a></li>
-      					
-      					<li><a href="mentionlegale.php">Infos légales</a></li>
-      					
-      					<li><a href="quisommesnous.php">Qui sommes-nous ?</a></li>
-      					
-      					<li><a href="conditions.php">Conditions générales de vente</a></li>
-      				</ul>
-      			</center>
-		      </footer>
-
-  </div>
 
 <div id="connex" style="visibility: hidden" name="connex">
 <h4>JE ME CONNECTE</h4>
